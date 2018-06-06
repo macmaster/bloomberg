4a979b79ed9f663e8e93541825e778337482c1e3
ronny-macmaster <ronnymacmaster@gmail.com>
2018-06-06 19:36:05 -0400

    modernize the overdose.sh script

--- overdose/overdose.sh
+++ overdose/overdose.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
 
-cat <<EOT > overdose.hsh
+ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -v|--verbose)
+      echo "verbose"
+      shift
+      ;;
+    -f|--file)
+      csv=$2
+      shift; shift
+      ;;
+    -c|--columns)
+      columns=$2
+      shift; shift
+      ;;
+    *)
+      ARGS+=($1)
+      shift
+      ;;
+  esac
+done
+
+set -- ${POSITIONAL[@]}
+
+csv=${csv-overdose.csv}
+columns=${columns-$(head -n1 $csv | sed -e 's/^,//')}
+
+echo "csv: $csv"
+echo "columns: $columns"
+
+# create the hbase table
+hbase shell <<<"
 disable 'overdose'
 drop 'overdose'
 create 'overdose', 'metadata', 'personal', 'residence', 'death', 'drug', 'manner'
 exit
-EOT
+"
 
-columns=`head -n1 overdose.csv` && columns=${columns#,}
-hdfs dfs -copyFromLocal -f overdose.csv /tmp/overdose.csv 
-hbase shell overdose.hsh
+# import the hbase table from hdfs
+overdose_csv="/tmp/$csv"
+hdfs dfs -copyFromLocal -f $csv $overdose_csv 
 hbase org.apache.hadoop.hbase.mapreduce.ImportTsv \
-  -Dimporttsv.separator=, -Dimporttsv.columns="HBASE_ROW_KEY,$columns" overdose hdfs://Test-Laptop:/tmp/overdose.csv
-
-rm overdose.hsh
+  -Dimporttsv.separator=, -Dimporttsv.columns="HBASE_ROW_KEY,$columns" overdose hdfs://Test-Laptop:$overdose_csv
